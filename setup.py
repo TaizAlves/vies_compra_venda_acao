5b62dd367c7331705247db1e8b3dfc9073587c60
taizalves <taizralves@gmail.com>
2022-03-30 19:37:46 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeção de Viés de compra e venda da AMBEV na B3 ',
+    author='team',
+    license='MIT',
+)
